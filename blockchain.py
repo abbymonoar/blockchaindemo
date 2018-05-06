27aa55980109a468ea2101636f35137cef521b7f
amonoar <abdullah.monoar@risepeople.com>
2018-05-06 00:40:34 -0400

    most functions added in

---
+++ blockchain.py
@@ -0,0 +1,67 @@
+import hashlib
+import json
+from time import time
+
+class Blockchain(object):
+	def __init__(self):
+			#constructor class variables
+			self.block_chain = []
+			self.current_transaction = []
+
+			#create the genesis block
+			self.new_block(1, 100)
+
+	def new_block(self, previous_hash=None, proof):
+		#creates new block in chain
+
+		#proof <int>  -- proof provided by 'Proof of work' algorithm
+		#previous_hash <str>  -- hash of previous block
+		
+		#return <dict> -- new block
+
+		block = {
+			'index': len(self.block_chain) + 1,
+			'timestamp': time(),
+			'transactions': self.current_transactions,
+			'proof': proof,
+			'previous_hash': previous_hash or self.hash(self.block_chain[-1])
+		}
+
+		#reset current list of transactions
+		self.current_transactions = []
+
+		self.block_chain.append(block)
+		return block
+		
+
+	def new_transaction(self, sender, recipient, memo):
+		#creates transaction for next mined block
+		
+		# sender 	<str> -- sender address
+		# recipient <str> -- recipient address 
+		# memo 		<str> -- transaction data
+
+		#return block index <int>
+
+		self.current_transactions.append({
+				'sender': sender,
+				'recipient': recipient,
+				'memo': memo,
+			})
+
+		return self.last_block['index'] + 1
+
+	@staticmethod
+	def hash(block):
+		#create a SHA-256 hash of a block
+
+		#block <dict> -- a block from block chain
+		#return <str> -- block hashed in hashing algorithm
+
+		block_string = json.dumps(block, sort_keys=True).encode()
+
+		return hashlib.sha256(block_string).hexdigest()
+
+	@property
+	def last_block(self):
+		return self.block_chain[-1]
